e449ecd7788929790d8c150acaa054b8bb9e1a83
Zhaojia-Liang <zliang@csumb.edu>
2020-09-15 23:19:40 -0700

    RegisterActivity

--- app/src/main/java/com/example/cst438_project01_group4/RegisterActivity.java
+++ app/src/main/java/com/example/cst438_project01_group4/RegisterActivity.java
@@ -1,10 +1,27 @@
 package com.example.cst438_project01_group4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.cst438_project01_group4.ClassObjects.User;
+import com.example.cst438_project01_group4.DataBase.AppDatabase;
+import com.example.cst438_project01_group4.DataBase.GradeAppDAO;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private GradeAppDAO mGradeAppDAO;
+    private EditText Newuser;
+    private EditText Newpassword;
+    private EditText Firstname;
+    private EditText Lastname;
+    private Button Sign = findViewById(R.id.btnSign);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +29,40 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        getGradeAppDAO();
+        Newuser = findViewById(R.id.etNewuser);
+        Newpassword = findViewById(R.id.etNewpassword);
+        Firstname = findViewById(R.id.etFirstname);
+        Lastname = findViewById(R.id.etLastname);
+
+        String username = Newuser.getText().toString();
+        String password = Newpassword.getText().toString();
+        String firstname = Firstname.getText().toString();
+        String lastname = Lastname.getText().toString();
+
+        Sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mGradeAppDAO.getAllUsers().isEmpty() || mGradeAppDAO.getUserByUsername(username) == null){
+                    User newuser = new User(username, password, firstname, lastname);
+                    mGradeAppDAO.insert(newuser);
+
+                    Intent intent = new Intent(RegisterActivity.this, ManageCourses.class);
+                    intent.putExtra("user", username);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Username already exists, please try again!", Toast.LENGTH_LONG).show();
+                    Newuser.setError("Incorrect username!");
+                }
+            }
+        });
+
+    }
+
+    private void getGradeAppDAO(){
+        mGradeAppDAO = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.DB_NAME)
+                .allowMainThreadQueries()
+                .build()
+                .getGradeAppDao();
     }
 }
